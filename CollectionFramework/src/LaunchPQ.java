541c8b80413f35bb85b09cd191d44e8c918f71ac
Tanishq Sharma <96770081+tanishq-1012@users.noreply.github.com>
2023-07-02 17:59:31 +0530

    CollectionFrameworkPrograms

---
+++ CollectionFramework/src/LaunchPQ.java
@@ -0,0 +1,22 @@
+//PriorityQueue Demonstration
+import java.util.*;
+public class LaunchPQ {
+
+	public static void main(String[] args) {
+		PriorityQueue pq = new PriorityQueue();
+		pq.add(10);
+		pq.add(200);
+		pq.add(25);
+		pq.add(75);
+		pq.add(100);
+		pq.add(120);
+		
+		//Priority queue give give us unordered list
+		System.out.println(pq);
+		
+		
+		
+
+	}
+
+}
